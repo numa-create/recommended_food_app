ddfd0aa0563750ed8a9e63f5f550ff66177d9a02
numa-create <k.numata0815@gmail.com>
2021-11-15 17:53:00 +0900

    いいね機能を非同期対応でエラー

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,12 @@
 class Food < ApplicationRecord
   belongs_to :user
+  has_many :likes, dependent: :destroy
+  # food.liked_users で food を「いいね!」しているユーザーの一覧を取得できるようになる
+  has_many :liked_users, through: :likes, source: :user
   validates :name, presence: true
+  # food を user が「いいね！」しているときは「true」，「いいね」していないときは「false」
+  def liked_by?(user)
+    likes.any? { |like| like.user_id == user.id }
+  end
   mount_uploader :image, ImageUploader
 end
